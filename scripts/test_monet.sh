01b6c3aaf4f2bb6832871f4a5ab3e1752968c9ef
Ruidong Wu <mooninrain@outlook.com>
2019-10-04 19:57:42 +0800

    Update test_monet.sh

--- scripts/test_monet.sh
+++ scripts/test_monet.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ./datasets/CLEVR_v1.0 --name clevr_monet --model monet --phase test
+python test.py --dataroot /data/vision/billf/jwu-recog/dataset/billf-10/CLEVR_v1.0 --name clevr_monet --model monet --phase test
