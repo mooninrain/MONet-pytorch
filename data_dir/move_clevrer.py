6b47ed1639dc8e8a0f597e7ce049ae4de773e14b
Ruidong Wu <mooninrain@outlook.com>
2019-10-11 04:10:33 +0800

    Update move_clevrer.py

--- data_dir/move_clevrer.py
+++ data_dir/move_clevrer.py
@@ -1,16 +1,29 @@
 import os
+import random
 import shutil
 
 ori_dir = '/data/vision/billf/scratch/kyi/projects/temporal-physics-reasoning/data/clevrer/ver1.0/frames'
 to_dir = '/data/vision/billf/scratch/ruidongwu/work/MONet-pytorch/data_dir/clevrer'
 to_move_dirs = os.listdir(ori_dir)
 to_move_dirs.sort()
+random.seed(123)
+train_dirs=random.sample(to_move_dirs,int(0.9*len(to_move_dirs)))
+val_dirs = []
+for _dir_ in to_move_dirs:
+    if _dir_ not in train_dirs:
+        val_dirs.append(_dir_)
 
 if not os.path.exists(to_dir):
     os.makedirs(to_dir)
 
-for k, _dir_ in enumerate(to_move_dirs):
+for k, _dir_ in enumerate(train_dirs):
     to_move_pngs = os.listdir(os.path.join(ori_dir,_dir_))
-    to_move_pngs.sort()
+    to_move_pngs = random.sample(to_move_pngs,1)
     for _png_ in to_move_pngs:
-        shutil.copy(os.path.join(ori_dir,_dir_,_png_),os.path.join(to_dir,_dir_+'_'+_png_))
+        shutil.copy(os.path.join(ori_dir,_dir_,_png_),os.path.join(to_dir,'train',_dir_+'_'+_png_))
+
+for k, _dir_ in enumerate(val_dirs):
+    to_move_pngs = os.listdir(os.path.join(ori_dir,_dir_))
+    to_move_pngs = random.sample(to_move_pngs,1)
+    for _png_ in to_move_pngs:
+        shutil.copy(os.path.join(ori_dir,_dir_,_png_),os.path.join(to_dir,'val',_dir_+'_'+_png_))
