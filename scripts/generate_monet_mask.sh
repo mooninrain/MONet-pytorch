d60fd5d4d360fe572c788041a9f06e2ce8ea669f
Ruidong Wu <mooninrain@outlook.com>
2019-10-12 15:42:15 +0800

    Update generate_monet_mask.sh

--- scripts/generate_monet_mask.sh
+++ scripts/generate_monet_mask.sh
@@ -1,2 +1,2 @@
 set -ex
-python generate_mask.py --dataroot /data/vision/billf/jwu-recog/dataset/billf-10/CLEVR_v1.0 --name clevr_monet --model monet --phase test
+jac-run generate_mask.py --dataroot /data/vision/billf/jwu-recog/dataset/billf-10/CLEVR_v1.0 --name clevr_monet --model monet --phase test
