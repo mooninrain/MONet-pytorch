467561821a1df03264dfbeaf005cd515c82c3d23
Ruidong Wu <mooninrain@outlook.com>
2019-10-25 19:51:39 +0800

    Update monet_model.py

--- models/monet_model.py
+++ models/monet_model.py
@@ -48,7 +48,7 @@ class MONetModel(BaseModel):
                             ['x', 'x_tilde']
         self.model_names = ['Attn', 'CVAE']
         self.netAttn = networks.init_net(networks.Attention(opt.input_nc, 1,load_size=opt.load_size), gpu_ids=self.gpu_ids)
-        self.netCVAE = networks.init_net(networks.ComponentVAE(opt.input_nc, opt.z_dim), gpu_ids=self.gpu_ids)
+        self.netCVAE = networks.init_net(networks.ComponentVAE(opt.input_nc, opt.z_dim, load_size=opt.load_size), gpu_ids=self.gpu_ids)
         self.eps = torch.finfo(torch.float).eps
         # define networks; you can use opt.isTrain to specify different behaviors for training and test.
         if self.isTrain:  # only defined during training time
