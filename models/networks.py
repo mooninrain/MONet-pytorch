008e333ee72ed93c63fdc051c174e12c7d06f88d
Ruidong Wu <mooninrain@outlook.com>
2019-10-25 19:50:04 +0800

    Update networks.py

--- models/networks.py
+++ models/networks.py
@@ -743,26 +743,30 @@ class Attention(nn.Module):
         self.downblock2 = AttentionBlock(ngf, ngf * 2)
         self.downblock3 = AttentionBlock(ngf * 2, ngf * 4)
         self.downblock4 = AttentionBlock(ngf * 4, ngf * 8)
-        self.downblock5 = AttentionBlock(ngf * 8, ngf * 8, resize=False)
+        self.downblock5 = AttentionBlock(ngf * 8, ngf * 16)
+        self.downblock6 = AttentionBlock(ngf * 16, ngf * 32)
+        self.downblock7 = AttentionBlock(ngf * 32, ngf * 32, resize=False)
         # no resizing occurs in the last block of each path
         # self.downblock6 = AttentionBlock(ngf * 8, ngf * 8, resize=False)
 
         self.mlp = nn.Sequential(
-            nn.Linear(int(load_size/16) * int(load_size/16) * ngf * 8, 128),
+            nn.Linear(int(load_size/16) * int(load_size/16) * ngf * 32, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(128, int(load_size/16) * int(load_size/16) * ngf * 8),
+            nn.Linear(128, int(load_size/16) * int(load_size/16) * ngf * 32),
             nn.ReLU(),
         )
 
         # self.upblock1 = AttentionBlock(2 * ngf * 8, ngf * 8)
-        self.upblock2 = AttentionBlock(2 * ngf * 8, ngf * 8)
-        self.upblock3 = AttentionBlock(2 * ngf * 8, ngf * 4)
-        self.upblock4 = AttentionBlock(2 * ngf * 4, ngf * 2)
-        self.upblock5 = AttentionBlock(2 * ngf * 2, ngf)
+        self.upblock2 = AttentionBlock(2 * ngf * 32, ngf * 32)
+        self.upblock3 = AttentionBlock(2 * ngf * 32, ngf * 16)
+        self.upblock4 = AttentionBlock(2 * ngf * 16, ngf * 8)
+        self.upblock5 = AttentionBlock(2 * ngf * 8, ngf * 4)
+        self.upblock6 = AttentionBlock(2 * ngf * 4, ngf * 2)
+        self.upblock7 = AttentionBlock(2 * ngf * 2, ngf)
         # no resizing occurs in the last block of each path
-        self.upblock6 = AttentionBlock(2 * ngf, ngf, resize=False)
+        self.upblock8 = AttentionBlock(2 * ngf, ngf, resize=False)
 
         self.output = nn.Conv2d(ngf, output_nc, 1)
 
@@ -773,21 +777,25 @@ class Attention(nn.Module):
         x, skip3 = self.downblock3(x)
         x, skip4 = self.downblock4(x)
         x, skip5 = self.downblock5(x)
-        skip6 = skip5
+        x, skip6 = self.downblock5(x)
+        x, skip7 = self.downblock5(x)
+        skip8 = skip7
         # The input to the MLP is the last skip tensor collected from the downsampling path (after flattening)
         # _, skip6 = self.downblock6(x)
         # Flatten
-        x = skip6.flatten(start_dim=1)
+        x = skip8.flatten(start_dim=1)
         x = self.mlp(x)
         # Reshape to match shape of last skip tensor
-        x = x.view(skip6.shape)
+        x = x.view(skip8.shape)
         # Upsampling blocks
         # x = self.upblock1(x, skip6)
-        x = self.upblock2(x, skip5)
-        x = self.upblock3(x, skip4)
-        x = self.upblock4(x, skip3)
-        x = self.upblock5(x, skip2)
-        x = self.upblock6(x, skip1)
+        x = self.upblock2(x, skip7)
+        x = self.upblock3(x, skip6)
+        x = self.upblock4(x, skip5)
+        x = self.upblock5(x, skip4)
+        x = self.upblock6(x, skip3)
+        x = self.upblock7(x, skip2)
+        x = self.upblock8(x, skip1)
         # Output layer
         x = self.output(x)
         x = F.logsigmoid(x)
